368b776a4aaf576e2db254d63d571dfc2cf8e082
xwidiiiaa2007 <vivoeka899@gmail.com>
2025-09-19 20:47:48 +0700

    ubah html dan js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,42 @@
-let todos =[];
+let todos = [];
 
-
-function addTodo(){
-    ///Get input values
+function addTodo() {
+    // Get input values
     const todoInput = document.getElementById("todo-input");
     const todoDate = document.getElementById("todo-date");
 
-    if (validateInput (todoInput.value, todoDate.value)){
-     
-        ///Add to array
+    if (validateInput(todoInput.value, todoDate.value)) {
+        // Add to array
         const FormTodo = { task: todoInput.value, date: todoDate.value };
         todos.push(FormTodo);
 
-        ///display updated todos
-        displayTodos
+        // Display updated todos
+        displayTodos();
 
-        /// clear input fields
+        // Clear input fields
         todoInput.value = "";
         todoDate.value = "";
-
-
     }
 }
 
-function displayTodos(){
-    ///Clear existing list
+function displayTodos() {
+    // Clear existing list
     const todoList = document.getElementById("todo-list");
-    todoList.innerHTML ="";
+    todoList.innerHTML = "";
 
-    ///Display all todos
-    todos.forEach((todo, _)=>{
-        todoList.innerHTML += `<li class="border-b border-grey-300 py-2 ">${todo.task} - <span class="text-sm text-gray-500">${todo.date}</span></li>`;
+    // Display all todos
+    todos.forEach((todo, _) => {
+        todoList.innerHTML += `<li class="border-b border-gray-300 py-2 ">${todo.task} - <span class="text-sm text-gray-500">${todo.date}</span></li>`;
     });
 }
 
-/// Mandate function for future development
-function deleteTodo(){ }
-/// Mandate function for future development
-function filterTodo(){ }
+// Mandate function for future development
+function deleteTodo() { }
+// Mandate function for future development
+function filterTodo() { }
 
-function validateInput(todo, date){ 
-    ///simple validation
+function validateInput(todo, date) {
+    // Simple validation
     if (todo === "" || date === "") {
         alert("Please fill in both fields.");
         return false;
