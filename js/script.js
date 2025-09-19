237a2cd45a114c1414460d4c154a819bab9c13eb
xwidiiiaa2007 <vivoeka899@gmail.com>
2025-09-19 13:37:45 +0700

    perubahan

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
 let todos =[];
 
 
-
 function addTodo(){
-
+    ///Get input values
     const todoInput = document.getElementById("todo-input");
     const todoDate = document.getElementById("todo-date");
 
-    console.log("Todo:", todoInput.value);
-    console.log("Date:", todoDate.value);
     if (validateInput (todoInput.value, todoDate.value)){
-        console.log("Hello World");
+     
+        ///Add to array
+        const FormTodo = { task: todoInput.value, date: todoDate.value };
+        todos.push(FormTodo);
+
+        ///display updated todos
+        displayTodos
+
+        /// clear input fields
+        todoInput.value = "";
+        todoDate.value = "";
+
 
     }
 }
 
+function displayTodos(){
+    ///Clear existing list
+    const todoList = document.getElementById("todo-list");
+    todoList.innerHTML ="";
 
-function displayTodos(){}
+    ///Display all todos
+    todos.forEach((todo, _)=>{
+        todoList.innerHTML += `<li class="border-b border-grey-300 py-2 ">${todo.task} - <span class="text-sm text-gray-500">${todo.date}</span></li>`;
+    });
+}
 
+/// Mandate function for future development
 function deleteTodo(){ }
-
+/// Mandate function for future development
 function filterTodo(){ }
 
-function validateInput(){ 
+function validateInput(todo, date){ 
+    ///simple validation
     if (todo === "" || date === "") {
-alert("Please fill in both fields.");
-return false
-   return true;
-}
+        alert("Please fill in both fields.");
+        return false;
     }
+    return true;
+}
